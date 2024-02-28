f3396142e2b03b74dddec4295065930598f467eb
aLiyevTamerlan <tamerlan-aliyev-2021@mail.ru>
2024-02-28 13:15:31 +0400

    null push

--- src/dashbord/routers/section_left_routers.py
+++ src/dashbord/routers/section_left_routers.py
@@ -13,11 +13,10 @@ router = APIRouter()
 
 
 @router.get("/metrics/", response_model=List[MetricsOutPut])
-def metrics(fight_date: str, action_name_id: int, action_name:ActionNameEnum, fighter_id: int, db: Session = Depends(get_db)):
+def metrics(fight_date: str, action_name_id: int, fighter_id: int, db: Session = Depends(get_db)):
     fight_date = tuple(list(map(int, fight_date.split(","))))
     params = {"fight_date": fight_date, "action_name_id": action_name_id, "fighter_id": fighter_id}
     response_list = []
-    print(action_name)
-    if action_name == ActionNameEnum.Takedown:
-        response_list = medal_left_dashbord_service.takedown_statistic(params=params, db=db)
+    
+    response_list = medal_left_dashbord_service.takedown_statistic(params=params, db=db)
     return response_list
